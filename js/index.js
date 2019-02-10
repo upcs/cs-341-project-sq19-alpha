cb76ae2d3e9ad4fb6c37544399040590014d120e
holbrook20 <holbrook20@up.edu>
2019-02-10 13:05:14 -0800

    Sets visibility
    Clicking log in now triggers the rest of the webpage to become visible

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 function myfunction() {
     document.write(sum(2,3));
 }
+
+//This function deletes the login box from the page
+//and sets the rest of the page to visible
 function skipFunction(){
-    var el = document.getElementById( 'loginId' );
-    el.parentNode.removeChild( el );
+    var loginBoxDiv = document.getElementById( 'loginId' );
+    loginBoxDiv.parentNode.removeChild( loginBoxDiv );
+
+    document.getElementById("mainWebpage").style.visibility= "visible" ;
+ 
 }
