be02efef7688d832e0467cd9b7efa063f95ed211
holbrook20 <holbrook20@up.edu>
2019-02-10 12:44:08 -0800

    Log in Box
    Added a log in box to the html, css, and js files. Currently dissapears  when login is clicked or when the skip log in is clicked.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 function myfunction() {
-	document.write(sum(2,3));
+    document.write(sum(2,3));
+}
+function skipFunction(){
+    var el = document.getElementById( 'loginId' );
+    el.parentNode.removeChild( el );
 }
