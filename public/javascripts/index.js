fbaa61af5a293c6e151d3f874c4e68e8fbcee747
TawnyMotoyama <motoyama20@up.edu>
2019-02-25 20:23:31 -0800

    testing updates

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,58 @@
+
+//-------------------------------------------------------------------------------------
 function sum(x,y) {
 	return x+y;
 }
 //has to match function name
 module.exports = sum;
+
+
+//-------------------------------------------------------------------------------------
+function goToHome() {
+	var username = document.getElementById('username');
+	var password = document.getElementById('password');
+	var validUsername = /^[a-z]*(.gov)$/;
+	var validPassword = /^[a-z]*(.gov)$/;
+	var usernameVal = username.value;
+	var passwordVal = password.value;
+	var loginForm = document.getElementById('loginForm');
+	var testBool = false;
+
+	if ((usernameVal.match(validUsername)) && (passwordVal.match(validPassword))) {
+		//alert('This is a valid username & password! (:');
+		testBool = true;
+		loginForm.action = "govHome.html";
+	}
+	else { // Otherwise...
+		alert('WARNING: Must enter a valid username AND password.');
+		testBool = false;
+	}
+	return testBool;
+}
+module.exports = goToHome;
+
+
+//-------------------------------------------------------------------------------------
+function submitZip(){
+	var zipcodeInput = document.getElementById('zipcode');
+	var validZipcode = /^[0-9]{5}$/;
+	var textValue=zipcodeInput.value;
+	var image = document.getElementById('testPic');
+
+	if (textValue.match(validZipcode)){ 
+		//alert('This is a valid zipcode! (:');
+		$("form").remove();
+		image.style.visibility = "visible";
+		$("#tempMessage").text("We are working on your route!");
+		$("#tempMessage").value("We are working on your route!");
+
+	}
+	else{ // Otherwise...
+		alert('WARNING: Must enter a zipcode.');
+	}
+	return true;
+}
+module.exports = submitZip;
 
 
 
--- public/javascripts/index.test.js
+++ public/javascripts/index.test.js
@@ -2,27 +2,61 @@
 //has to match file name
 const sum = require('./index.js');
 
-describe('sum suite', function() {
+describe('sum suite', function () {
 	test('add 1+2 to equal 3', () => {
-		expect(sum(1,2)).toBe(3);
+		expect(sum(1, 2)).toEqual(3);
 	});
 
 	test('add 12+1 to equal 13', () => {
-		expect(sum(12,1)).toBe(13);
+		expect(sum(12, 1)).toBe(13);
 	});
 
 	test('add 38+62 to equal 100', () => {
-		expect(sum(38,62)).toBe(100);
+		expect(sum(38, 62)).toBe(100);
 	});
 });
 
-// const skipFunction = require('./index.js');
+//const goToHome = require('./index.js');
+const $ = require('jquery');
+jest.mock('./index');
+test('check for valid username & password', () => {
 
-// describe('skipFunction', function() {
-// 	test('loginBoxDiv should be null', () => {
-// 		expect(loginBoxDiv).toBeNull;
-// 	});
-// });
+	const goToHome = require('./index');
+
+	// Tell the fetchCurrentUser mock function to automatically invoke
+	// its callback with some data
+	goToHome.mockImplementation(cb => {
+		cb({
+			usernameVal: 'g.gov',
+			passwordVal: 'g.gov',
+			testBool: true
+		});
+	});
+
+	$("#loginButton").click();
+	expect(goToHome).toBeCalled();
+
+});
+
+
+
+jest.mock('./index');
+test('check for valid zipcode', () => {
+
+	const submitZip = require('./index');
+
+	// Tell the fetchCurrentUser mock function to automatically invoke
+	// its callback with some data
+	submitZip.mockImplementation(cb => {
+		cb({
+			textValue: '12345'
+		});
+	});
+
+	$("#submitZipButton").click();
+	expect(submitZip).toBeCalled();
+
+});
 
 
 
