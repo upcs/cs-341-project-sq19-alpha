421ae24b4734e344789fe2eb5b7d28e75974cfdb
holbrook20 <holbrook20@up.edu>
2019-02-26 18:31:48 -0800

    Changes made for Database
    -Added routes/orders
    -Orders recieves posts from the tableFunction.js file and uses sql to give database information.

---
+++ public/javascripts/tableFunction.js
@@ -0,0 +1,38 @@
+function dataFunction(){
+    //alert("hi");
+    
+    $.post("/orders",null,
+	   function(data, status){
+
+	       
+	       //Here we make a variable called tr that we will append into the
+	       //html file. Basically we are writing html code here and looping through
+	       //the content of the database.
+	       
+	       var tr = " ";
+	       
+	       for (var i = 0; i<data.length; i++)
+	       {
+		  tr += "<tr>"+"<td>" + data[i].address + "</td>"+"</tr>"; 
+	       }
+	       
+	       $("#myData").append(tr);
+	       
+	       alert("hi");
+	       
+	   }, "json");
+    
+}
+
+function main(){
+    $("#dataButton").on('click', dataFunction);
+}
+
+
+/* End of file */
+
+
+
+
+
+   
