28fb8aa3c6013924f8b656b7239330a1ee2cb12b
Sizhe Cao <cao20@up.edu>
2019-04-03 17:41:48 -0700

    final commit

--- public/javascripts/map.test.js
+++ public/javascripts/map.test.js
@@ -20,3 +20,24 @@
 //     window.google = google;
 // };
 // }
+
+
+
+
+
+const map = require('./map');
+
+describe('sum suite', function () {
+	test('add 1+2 to equal 3', () => {
+		expect(index.sum(1, 2)).toBe(3);
+	});
+
+	test('add 12+1 to equal 13', () => {
+		expect(index.sum(12, 1)).toBe(13);
+	});
+
+	test('add 38+62 to equal 100', () => {
+		expect(index.sum(38, 62)).toBe(100);
+	});
+});
+
