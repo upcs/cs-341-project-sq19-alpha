27cd9fe0e743e650f0a63d487f3b51fce5e717e3
holbrook20 <holbrook20@up.edu>
2019-04-24 23:26:32 -0700

    Added more edge cases
    More edge cases

--- public/javascripts/login.test.js
+++ public/javascripts/login.test.js
@@ -1,21 +1,88 @@
 const login = require('./login');
 
 describe('isValidUnamePass suite', function () {
-	test('test valid uname & pass', () => {
+	test('test valid username & password', () => {
 		expect(login.isValidUnamePass('test@test.gov', 'tester')).toBe(true);
 	});
 
-	test('test invalid pass', () => {
-		expect(login.isValidUnamePass('fail.gov', '123')).toBe(false);
+    	test('test valid username & password', () => {
+		expect(login.isValidUnamePass('test@.gov', 'tester')).toBe(true);
 	});
 
-	test('test invalid uname', () => {
+    	test('test valid username & password', () => {
+		expect(login.isValidUnamePass('test.gov', 'tester')).toBe(true);
+	});
+
+    	test('test valid username & password', () => {
+		expect(login.isValidUnamePass('test@gov', 'tester')).toBe(true);
+	});
+
+    	test('test valid username & password', () => {
+		expect(login.isValidUnamePass('testgov', 'tester')).toBe(true);
+	});
+
+	test('test password too short', () => {
+		expect(login.isValidUnamePass('test@test.gov', 'hey')).toBe(false);
+	});
+
+    	test('test password too long', () => {
+		expect(login.isValidUnamePass('test@test.gov', 'heyheyheyheyheyhey')).toBe(false);
+	});
+
+	test('test invalid username', () => {
 		expect(login.isValidUnamePass('jimmy', 'cole.gov')).toBe(false);
 	});
 
 	test('test empty strings', () => {
 		expect(login.isValidUnamePass('', '')).toBe(false);
 	});
+    
+    	test('Just empty password', () => {
+		expect(login.isValidUnamePass('test@test.gov', '')).toBe(false);
+	});
+    
+    	test('Just empty username', () => {
+	    expect(login.isValidUnamePass('', 'tester')).toBe(false);    
+	});
+
+    	test('Symbols in username', () => {
+		expect(login.isValidUnamePass('tester!@test.gov', 'tester')).toBe(false);
+	});
+
+    	test('Symbols in the password', () => {
+		expect(login.isValidUnamePass('tester@test.gov', 'te$ter')).toBe(false);
+	});
+
+    	test('Space in the password', () => {
+		expect(login.isValidUnamePass('tester@test.gov', 'te ster')).toBe(false);
+	});
+    
+    	test('Space before the password', () => {
+		expect(login.isValidUnamePass('tester@test.gov', ' tester')).toBe(false);
+	});
+    
+    	test('Space after the password', () => {
+		expect(login.isValidUnamePass('tester@test.gov', 'tester ')).toBe(false);
+	});
+
+    	test('Space in the username', () => {
+		expect(login.isValidUnamePass('tester @test.gov', 'te ster')).toBe(false);
+	});
+    
+    	test('Space before the username', () => {
+		expect(login.isValidUnamePass(' tester@test.gov', 'tester')).toBe(false);
+	});
+    
+    	test('Space after the username', () => {
+		expect(login.isValidUnamePass('tester@test.gov ', 'tester')).toBe(false);
+	});
+
+    
+    	test('@ in the password', () => {
+		expect(login.isValidUnamePass('tester@test.gov', 'te@ster')).toBe(false);
+	});
+
+  
 
 	// test("returns undefined by default", () => {
 
--- public/javascripts/register.test.js
+++ public/javascripts/register.test.js
@@ -3,56 +3,105 @@ const register = require('./register');
 
 describe('isValidUsername suite', function () {
 	test('test valid uname', () => {
-		expect(register.isValidUsername('g@ah.gov')).toBe(true);
+		expect(register.isValidUsername('test@test.gov')).toBe(true);
 	});
 
 	test('test valid uname', () => {
-		expect(register.isValidUsername('asdf@up.gov')).toBe(true);
+		expect(register.isValidUsername('hi@h.gov')).toBe(true);
+	});
+    
+    	test('test not a gov', () => {
+		expect(register.isValidUsername('test@test.com')).toBe(false);
 	});
 
-	test('test valid uname', () => {
-		expect(register.isValidUsername('asd')).toBe(false);
+	test('test no @ symbol', () => {
+		expect(register.isValidUsername('testing')).toBe(false);
 	});
 
-	test('test valid uname', () => {
-		expect(register.isValidUsername('123')).toBe(false);
+	test('test nothing before the @ symbol', () => {
+		expect(register.isValidUsername('@hi')).toBe(false);
+	});
+    
+    	test('only @ and gov', () => {
+		expect(register.isValidUsername('@gov')).toBe(false);
+	});
+    
+        test('only @ and gov', () => {
+		expect(register.isValidUsername('@.gov')).toBe(true);
+	});
+    
+    	test('test empty string', () => {
+		expect(register.isValidUsername('')).toBe(false);
+	});
+    
+    	test('test contains symbols', () => {
+		expect(register.isValidUsername('test!@test.gov')).toBe(false);
+	});
+    
+    	test('test contains a space', () => {
+		expect(register.isValidUsername('test!@ test.gov')).toBe(false);
 	});
 });
 
 
 describe('isValidPassword suite', function () {
 	test('test valid password', () => {
-		expect(register.isValidPassword('asdfgh')).toBe(true);
+		expect(register.isValidPassword('password')).toBe(true);
 	});
 
-	test('test valid password', () => {
-		expect(register.isValidPassword('asd123')).toBe(true);
+	test('test valid password with numbers', () => {
+		expect(register.isValidPassword('password123')).toBe(true);
 	});
 
-	test('test valid password', () => {
-		expect(register.isValidPassword('asd')).toBe(false);
+	test('test password too short', () => {
+		expect(register.isValidPassword('pass')).toBe(false);
 	});
 
-	test('test valid password', () => {
-		expect(register.isValidPassword('123')).toBe(false);
+	test('test password too long', () => {
+		expect(register.isValidPassword('passwordpassword')).toBe(false);
 	});
+
+       	test('test password with symbols', () => {
+		expect(register.isValidPassword('password!')).toBe(false);
+	});
+
+    	test('test empty password', () => {
+		expect(register.isValidPassword('')).toBe(false);
+	});
+    
+    	test('test password with a space', () => {
+		expect(register.isValidPassword('pass word')).toBe(false);
+	});
+    
 });
 
 describe('isValidRepPsw suite', function () {
-	test('test valid password', () => {
-		expect(register.isValidRepPsw('asdfg', 'asdfg')).toBe(true);
+        test('test valid password', () => {
+	    expect(register.isValidPassword('password')).toBe(true);
 	});
 
-	test('test valid password', () => {
-		expect(register.isValidRepPsw('asd123', 'asd123')).toBe(true);
+	test('test valid password with numbers', () => {
+		expect(register.isValidPassword('password123')).toBe(true);
 	});
 
-	test('test valid password', () => {
-		expect(register.isValidRepPsw('asddf', '12345')).toBe(false);
+	test('test password too short', () => {
+		expect(register.isValidPassword('pass')).toBe(false);
 	});
 
-	test('test valid password', () => {
-		expect(register.isValidRepPsw('123asd', 'asd123')).toBe(false);
+	test('test password too long', () => {
+		expect(register.isValidPassword('passwordpassword')).toBe(false);
+	});
+    
+       	test('test password with symbols', () => {
+		expect(register.isValidPassword('password!')).toBe(false);
+	});
+
+    	test('test empty password', () => {
+		expect(register.isValidPassword('')).toBe(false);
+	});
+    
+    	test('test password with a space', () => {
+		expect(register.isValidPassword('pass word')).toBe(false);
 	});
 });
 
